24a6459ebfd2de52199d27f94a4cc89cb47b1a10
quguocheng <quguocheng@meituan.com>
2015-12-16 11:33:20 +0800

    添加两个test case： test_removeNodeWithTwoChilds test_removeNodeWithLeftChildHavingRightTree

--- test/TestRemoveNode.groovy
+++ test/TestRemoveNode.groovy
@@ -1,7 +1,10 @@
 import org.testng.annotations.BeforeMethod
 import org.testng.annotations.Test
+
 import tree.binary.CommonOperation
 import tree.binary.Node
+
+import org.testng.Assert
 
 class TestRemoveNode{
     Node root
@@ -15,7 +18,19 @@ class TestRemoveNode{
                             value:5
                         ),
                     right:new Node(
-                            value:15
+                            value:15,
+                            left:new Node(
+                                    value:11,
+                                    right:new Node(
+                                            value:12,
+                                            right:new Node(
+                                                    value:13
+                                                )
+                                        )
+                                ),
+                            right:new Node(
+                                    value:16
+                                )
                         )
                 ),
             right:new Node(
@@ -32,21 +47,40 @@ class TestRemoveNode{
     @Test
     def void test_removeRoot(){
         def( newRoot,replaceNode)=CommonOperation.removeNodeFromTree(root, new Node(value:20))
-        println newRoot.value
-        assert newRoot.value==15
+        Assert.assertEquals(newRoot.value, 16)
     }
     
     @Test
     def void test_removeNodeWithOnlyLeftChild(){
         def (newRoot,replaceNode)=CommonOperation.removeNodeFromTree(root,new Node(value:30))
-        assert newRoot.value==20
-        assert replaceNode.value==25
+        Assert.assertEquals(newRoot.value,20)
+        Assert.assertEquals(replaceNode.value,25)
     }
     
     @Test
     def void test_removeNodeWithOnlyRightChild(){
         def (newRoot, replaceNode)=CommonOperation.removeNodeFromTree(root, new Node(value:25))
-        assert newRoot.value==20
-        assert replaceNode.value==27
+        Assert.assertEquals(newRoot.value,20)
+        Assert.assertEquals(replaceNode.value, 27)
+    }
+    
+    /**
+     * 被删除的节点10，左右子树都有，而且其left child没有右子树
+     */
+    @Test
+    def void test_removeNodeWithTwoChilds(){
+        def (newRoot, replaceNode)=CommonOperation.removeNodeFromTree(root,new Node(value:10))
+        Assert.assertEquals(newRoot.value,20)
+        Assert.assertEquals(replaceNode.value, 5)
+    }
+    
+    /**
+     * 被删除的节点15，左右子树都有，而且其left child有右子树
+     */
+    @Test
+    def void test_removeNodeWithLeftChildHavingRightTree(){
+        def (newRoot, replaceNode)=CommonOperation.removeNodeFromTree(root,new Node(value:15))
+        Assert.assertEquals(newRoot.value,20)
+        Assert.assertEquals(replaceNode.value, 13)
     }
 }
