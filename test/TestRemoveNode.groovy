d08892487f5d957cea40478c8eb354791e298ec3
quguocheng <quguocheng@meituan.com>
2015-12-16 11:16:42 +0800

    添加test_removeNodeWithOnlyLeftChild，test_removeNodeWithOnlyRightChild

--- test/TestRemoveNode.groovy
+++ test/TestRemoveNode.groovy
@@ -12,28 +12,41 @@ class TestRemoveNode{
             left:new Node(
                     value:10,
                     left:new Node(
-                            value:5,
-                            left:null,
-                            right:null
+                            value:5
                         ),
                     right:new Node(
-                            value:15,
-                            left:null,
-                            right:null
+                            value:15
                         )
                 ),
             right:new Node(
                     value:30,
-                    left:null,
-                    right:null
+                    left:new Node(
+                            value:25,
+                            right:new Node(
+                                    value:27
+                                )
+                        )
                 )
         )
     }
     @Test
     def void test_removeRoot(){
-        setUp()
-        Node newRoot=CommonOperation.removeNodeFromTree(root, new Node(value:20))
+        def( newRoot,replaceNode)=CommonOperation.removeNodeFromTree(root, new Node(value:20))
         println newRoot.value
         assert newRoot.value==15
+    }
+    
+    @Test
+    def void test_removeNodeWithOnlyLeftChild(){
+        def (newRoot,replaceNode)=CommonOperation.removeNodeFromTree(root,new Node(value:30))
+        assert newRoot.value==20
+        assert replaceNode.value==25
+    }
+    
+    @Test
+    def void test_removeNodeWithOnlyRightChild(){
+        def (newRoot, replaceNode)=CommonOperation.removeNodeFromTree(root, new Node(value:25))
+        assert newRoot.value==20
+        assert replaceNode.value==27
     }
 }
